7569ba9125fad01f5b1e58f6f6f7009ab37427f4
AlexBizPlus <alexeynejnov@gmail.com>
2020-08-24 19:45:30 +0700

    homework #4

--- main.js
+++ main.js
@@ -6,12 +6,34 @@ const MAX_DAMAGE_SUPER = 25;
 const $buttonThiderJolt = document.getElementById('btn-kick');
 const $buttonSuperStrike = document.getElementById('btn-kick-super');
 
+const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+function damageFighter(mimDamage, maxDamage) {
+  this.currentHealth -= getRandomInteger(mimDamage, maxDamage);
+
+  if (this.currentHealth < 0) {
+    this.currentHealth = 0;
+  }
+};
+
+function renderHealthState() {
+  this.progressbar.style.width = this.currentHealth / this.health * 100 + '%';
+  this.state.textContent = `${this.currentHealth} / ${this.health}`;
+};
+
 const hero = {
   name: 'Pikachu',
   health: 100,
   currentHealth: 100,
   progressbar: document.getElementById('progressbar-character'),
-  state: document.getElementById('health-character')
+  state: document.getElementById('health-character'),
+  damageFighter: damageFighter,
+  renderHealthState: renderHealthState
 };
 
 const enemy = {
@@ -19,14 +41,9 @@ const enemy = {
   health: 100,
   currentHealth: 100,
   progressbar: document.getElementById('progressbar-enemy'),
-  state: document.getElementById('health-enemy')
-};
-
-const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
+  state: document.getElementById('health-enemy'),
+  damageFighter: damageFighter,
+  renderHealthState: renderHealthState
 };
 
 const stopGame = () => {
@@ -46,22 +63,7 @@ const stopGame = () => {
   }
 
   renderMessage(message);
-  // alert(message);
-  // console.log(message);
 }
-
-const damageFighter = (person, mimDamage, maxDamage) => {
-  person.currentHealth -= getRandomInteger(mimDamage, maxDamage);
-
-  if (person.currentHealth <= 0) {
-    person.currentHealth = 0;
-  }
-};
-
-const renderHealthState = (person) => {
-  person.progressbar.style.width = person.currentHealth / person.health * 100 + '%';
-  person.state.textContent = `${person.currentHealth} / ${person.health}`;
-};
 
 const renderMessage = (message) => {
   const $header = document.querySelector('header');
@@ -71,18 +73,18 @@ const renderMessage = (message) => {
 };
 
 const buttonThiderJoltHandler = () => {
-  damageFighter(hero, MIN_DAMAGE_REGULAR, MAX_DAMAGE_REGULAR);
-  damageFighter(enemy, MIN_DAMAGE_REGULAR, MAX_DAMAGE_REGULAR);
-  renderHealthState(hero);
-  renderHealthState(enemy);
+  hero.damageFighter(MIN_DAMAGE_REGULAR, MAX_DAMAGE_REGULAR);
+  enemy.damageFighter(MIN_DAMAGE_REGULAR, MAX_DAMAGE_REGULAR);
+  hero.renderHealthState();
+  enemy.renderHealthState();
   if (hero.currentHealth === 0 || enemy.currentHealth === 0) {
     stopGame();
   }
 };
 
 const buttonSuperStrikeHandler = () => {
-  damageFighter(enemy, MIN_DAMAGE_SUPER, MAX_DAMAGE_SUPER);
-  renderHealthState(enemy);
+  enemy.damageFighter(MIN_DAMAGE_SUPER, MAX_DAMAGE_SUPER);
+  enemy.renderHealthState();
 
   if (enemy.currentHealth === 0) {
     stopGame();

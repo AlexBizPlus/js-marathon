e91a9025e75b61413fd8bb6286d0e18c8bef9327
AlexBizPlus <alexeynejnov@gmail.com>
2020-08-29 23:18:31 +0700

    homework

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ const MIN_DAMAGE_REGULAR = 5;
 const MAX_DAMAGE_REGULAR = 15;
 const MIN_DAMAGE_SUPER = 15;
 const MAX_DAMAGE_SUPER = 25;
+const MAX_LOG_ELEMENTS = 4;
+const MAX_CLICK_PER_BUTTON = 6;
+const BUTTONS_COUNT = 3;
 
 const $buttonThiderJolt = document.getElementById('btn-kick');
 const $buttonSuperStrike = document.getElementById('btn-kick-super');
+const $buttonRandom = document.getElementById('btn-kick-random');
 const $playground = document.querySelector('.playground');
 
 const hero = {
@@ -27,6 +31,24 @@ const enemy = {
   damageFighter: damageFighter,
   renderHealthState: renderHealthState
 };
+
+const clicks = {
+  maxClicks: MAX_CLICK_PER_BUTTON,
+  total: {
+    clickCount: 0,
+  },
+};
+
+const createButtons = (name) => {
+  const elem = Object.create(clicks);
+  elem.count = 0;
+  elem.name = name;
+  return elem;
+};
+
+const clickThiderJolt = createButtons('ThiderJolt');
+const clickSuperStrike = createButtons('SuperStrike');
+const clickRandom = createButtons('Random');
 
 const {
   name: nameHero,
@@ -84,33 +106,105 @@ const createLogMessage = (firstChar, secondChar, damage) => {
 
 // ------------------ render.js ----------------------------
 
-const caption = 'Fight log:'
+const caption = 'Fight log:';
+const clickCaption = 'Click log:';
 
-const renderLogContainer = () => {
+const renderContainer = () => {
   const logContainer = document.createElement('div');
-  logContainer.style.width = '250px';
+  logContainer.id = 'log-container';
+  logContainer.style.maxWidth = '1024px';
+  logContainer.style.display = 'flex';
+  logContainer.style.flexDirection = 'row';
+  logContainer.style.justifyContent = 'center';
+  logContainer.style.alignItems = 'flex-start';
+  logContainer.style.marginLeft = 'auto';
+  logContainer.style.marginRight = 'auto';
+
+  $playground.after(logContainer);
+  renderLogContainer(caption, MAX_LOG_ELEMENTS);
+  renderLogContainer(clickCaption, BUTTONS_COUNT + 2);
+  logContainer.children[logContainer.children.length - 1].style.marginRight = '0';
+  prepareLog();
+  prepareClickLog();
+};
+
+const prepareLog = () => {
+  const logContainer = document.querySelector('#log-container').children[0];
+  const logText = logContainer.querySelector('span');
+  logText.id = 'log-caption-text';
+};
+
+const prepareClickLog = () => {
+  const logContainer = document.querySelector('#log-container').children[1];
+
+  const logText = logContainer.querySelector('ul').children[0];
+  logText.innerText = `
+  Max ${MAX_CLICK_PER_BUTTON} clicks on one button!`;
+
+  const logButtonOne = logContainer.querySelector('ul').children[1];
+  logButtonOne.innerText = `
+  ${clickThiderJolt.name}:  `;
+  logButtonOne.id = clickThiderJolt.name;
+
+  const logButtonTwo = logContainer.querySelector('ul').children[2];
+  logButtonTwo.innerText = `
+  ${clickSuperStrike.name}:  `;
+  logButtonTwo.id = clickSuperStrike.name;
+
+  const logButtonThree = logContainer.querySelector('ul').children[3];
+  logButtonThree.innerText = `
+  ${clickRandom.name}:  `;
+  logButtonThree.id = clickRandom.name;
+
+  renderTotalClicks();
+};
+
+const renderButtonClicks = (id, total) => {
+  const logContainer = document.querySelector('#log-container').children[1];
+  const logButton = logContainer.querySelector(`#${id}`);
+  logButton.innerText = `
+  ${id}: ${total}`;
+}
+
+const renderTotalClicks = (total = 0) => {
+  const logContainer = document.querySelector('#log-container').children[1];
+  const logTotal = logContainer.querySelector('ul').children[BUTTONS_COUNT + 1];
+  logTotal.innerText = `
+  Total clicks: ${total}`;
+};
+
+const renderLogContainer = (text, index) => {
+  const logContainer = document.createElement('div');
+  logContainer.style.width = '350px';
   logContainer.classList.add('log-background');
   logContainer.style.marginRight = '20px';
-  logContainer.style.marginLeft = '0';
-  logContainer.style.marginTop = '20px';
-  logContainer.style.marginBottom = '20px';
 
   const logCaption = document.createElement('p');
-  logCaption.innerText = `${caption}`;
+  const logCaptionText = document.createElement('span');
+  logCaptionText.innerText = `${text}`;
+
+  const logList = document.createElement('ul');
+  for (i = 0; i < index; i++) {
+    const logText = document.createElement('li');
+    logList.append(logText);
+  }
 
   logContainer.append(logCaption);
-  $playground.append(logContainer);
+  logCaption.append(logCaptionText);
+  logCaption.append(logList);
+
+  const $log = document.querySelector('#log-container');
+  $log.append(logContainer);
 };
 
 const renderLogMessage = () => {
-  const $log = document.querySelector('.log-background');
-  const logText = document.createElement('span');
+  const $log = document.querySelector('.log-background ul');
 
-  logText.innerText = `
-  ${logList[logList.length - 1]}
-  `;
-
-  $log.prepend(logText);
+  for (i = 0; i < Math.min(logList.length, MAX_LOG_ELEMENTS); i++) {
+    $log.children[i].innerText = `
+      ${logList[logList.length - 1 - i]}
+    `;
+  }
 };
 
 function renderHealthState() {
@@ -119,7 +213,7 @@ function renderHealthState() {
 };
 
 const renderMessage = (message) => {
-  const $logCaption = document.querySelector('.log-background p');
+  const $logCaption = document.querySelector('#log-caption-text');
   $logCaption.innerText = `${message}`;
   $logCaption.style.backgroundColor = '#000000';
   $logCaption.style.color = '#ffffff';
@@ -144,6 +238,7 @@ function damageFighter(mimDamage, maxDamage) {
 const stopGame = () => {
   $buttonThiderJolt.disabled = true;
   $buttonSuperStrike.disabled = true;
+  $buttonRandom.disabled = true;
   let message;
 
   switch (true) {
@@ -166,6 +261,17 @@ const buttonThiderJoltHandler = () => {
   enemy.damageFighter(MIN_DAMAGE_REGULAR, MAX_DAMAGE_REGULAR);
   hero.renderHealthState();
   enemy.renderHealthState();
+
+  clickThiderJolt.count++;
+  clickThiderJolt.total.clickCount++;
+
+  clickThiderJolt.count >= clickThiderJolt.maxClicks ?
+    $buttonThiderJolt.disabled = true :
+    null;
+
+  renderButtonClicks(clickThiderJolt.name, clickThiderJolt.count);
+  renderTotalClicks(clickThiderJolt.total.clickCount);
+
   if (hero.currentHealth === 0 || enemy.currentHealth === 0) {
     stopGame();
   }
@@ -175,6 +281,12 @@ const buttonSuperStrikeHandler = () => {
   enemy.damageFighter(MIN_DAMAGE_SUPER, MAX_DAMAGE_SUPER);
   enemy.renderHealthState();
 
+  clickSuperStrike.count++;
+  clickSuperStrike.total.clickCount++;
+
+  renderButtonClicks(clickSuperStrike.name, clickSuperStrike.count);
+  renderTotalClicks(clickSuperStrike.total.clickCount);
+
   if (enemy.currentHealth === 0) {
     stopGame();
   }
@@ -182,11 +294,35 @@ const buttonSuperStrikeHandler = () => {
   $buttonSuperStrike.disabled = true;
 };
 
+const buttonRandomHandler = () => {
+  const randomChar = getRandomInteger() ?
+    hero.damageFighter(MIN_DAMAGE_SUPER, MAX_DAMAGE_SUPER) :
+    enemy.damageFighter(MIN_DAMAGE_SUPER, MAX_DAMAGE_SUPER);
+
+  hero.renderHealthState();
+  enemy.renderHealthState();
+
+  clickRandom.count++;
+  clickRandom.total.clickCount++;
+
+  clickRandom.count >= clickRandom.maxClicks ?
+    $buttonRandom.disabled = true :
+    null;
+
+  renderButtonClicks(clickRandom.name, clickRandom.count);
+  renderTotalClicks(clickRandom.total.clickCount);
+
+  if (hero.currentHealth === 0 || enemy.currentHealth === 0) {
+    stopGame();
+  }
+};
+
 const startGame = () => {
   $buttonThiderJolt.addEventListener('click', buttonThiderJoltHandler);
   $buttonSuperStrike.addEventListener('click', buttonSuperStrikeHandler);
+  $buttonRandom.addEventListener('click', buttonRandomHandler);
 
-  renderLogContainer();
+  renderContainer();
 };
 
 startGame();
